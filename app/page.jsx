daad7b550bd2cc408a87c106e2f2325affcac8c4
Nicholas Gabriel Gonzales <nina.gonzales.up@phinmaed.com>
2025-01-15 16:38:49 +0800

    refact:reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus doloribus qui, deserunt tempora magni maxime commodi eum blanditiis. Iusto laboriosam asperiores sequi adipisci corporis suscipit odio molestiae illo aperiam voluptatem.</p>
+    </div>
+  )
+}
+
+export default HomePage
